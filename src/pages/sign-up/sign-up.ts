ec3aeb25ee9c6b2a786f3992992ffc56a413b41f
SMSC <chathumal.sasanga02@gmail.com>
2018-05-03 21:41:13 +0530

    setup validation to sign up view and add styling to validation.

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthDataProvider } from '../../providers/auth-data/auth-data';
 
@@ -18,6 +19,8 @@ import { AuthDataProvider } from '../../providers/auth-data/auth-data';
 export class SignUpPage {
 
   dateRegex:any = /(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/;
+  emailRegex:any = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+  signUpForm:any;
 
   user:any = {
     "firstName": "",
@@ -30,11 +33,22 @@ export class SignUpPage {
   password:string;
   confirmPassword:string;
 
-  constructor(public navCtrl: NavController, public platform: Platform, public _authDataService: AuthDataProvider) {
+  constructor(public navCtrl: NavController, public platform: Platform, public _authDataService: AuthDataProvider, public formBuilder: FormBuilder) {
     let backAction =  platform.registerBackButtonAction(() => {
       this.navCtrl.pop();
       backAction();
-    },2)
+    },2);
+
+    this.signUpForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ['', Validators.compose([Validators.pattern(this.emailRegex), Validators.required])],
+      dob: ['', Validators.compose([Validators.pattern('/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/'), Validators.required])],
+      address: ['', Validators.compose([Validators.required])],
+      password: ['',Validators.compose([Validators.minLength(6), Validators.required])],
+      confirmPassword: ['',Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+
   }
 
   ionViewDidLoad() {
@@ -46,12 +60,16 @@ export class SignUpPage {
   }
 
   signUp () {
-    if (this.checkDateOfBirth()) {
+    if (this.signUpForm.valid) {
       if (this.checkPasswords()) {
         this.user.password = this.confirmPassword;
         let signUp = this._authDataService.registerUser(this.user);
         console.log("return", signUp);
+      } else {
+
       }
+    } else {
+
     }
   }
 
